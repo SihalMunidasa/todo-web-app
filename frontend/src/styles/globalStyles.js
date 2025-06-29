56fbb7270913229e6c131051b30e8605d32fe402
Sihal Munidasa <sihal.munidasa03@gmail.com>
2025-06-30 01:24:15 +0530

    Setup Global Styles

---
+++ frontend/src/styles/globalStyles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #f5f5f5;
+    color: #333;
+    line-height: 1.6;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .MuiSnackbar-root {
+    top: 70px !important;
+  }
+`;
+
+export default GlobalStyles;
